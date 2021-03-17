32ffd567c01ef482792578468e4e658251cd1d01
@lukeocodes <luke@lukeoliff.com>
2021-03-17 12:59:33 +0000

    insert generic commit message here

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Express' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
